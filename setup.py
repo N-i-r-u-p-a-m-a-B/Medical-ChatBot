70875bbc8a72fdbb315582b1ddd251b47738bb71
N-i-r-u-p-a-m-a-B <nithyabasavanna57@gmail.com>
2025-11-18 15:44:43 +0530

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Medical_ChatBot",
+    version="0.1.0",
+    author ="Nirupama B",
+    author_email = "nirupamabasavanna@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
+
